02d5d931e7b78a494cbc86ca022f0013298f117f
bilal <51857159+xeikh11@users.noreply.github.com>
2025-08-16 00:55:39 +0500

    tweaks

--- android/app/src/main/kotlin/com/example/face_detection_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/face_detection_app/MainActivity.kt
@@ -1,5 +1,33 @@
 package com.example.face_detection_app
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import androidx.annotation.NonNull // ✅ Correct import
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    private fun _getNativeLibDir(): String {
+        return applicationInfo.nativeLibraryDir
+    }
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) { // ✅ Correct keyword
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL)
+            .setMethodCallHandler { call, result ->
+                if (call.method == "getNativeLibDir") {
+                    val nativeLibDir: String = _getNativeLibDir()
+                    result.success(nativeLibDir)
+                } else {
+                    result.notImplemented()
+                }
+            }
+    }
+
+    companion object {
+        init {
+            System.loadLibrary("facerec")
+        }
+
+        private const val CHANNEL: String = "samples.flutter.dev/facesdk"
+    }
+}
